50d59942b85ce3a27c746ead17a1fc9b41a9411d
mbusila-unity3d <marian.busila@unity3d.com>
2022-12-22 13:54:51 -0500

    use Configuration annotation for DI instead of xml based

--- SpringFrameworkDemo/src/main/java/demo/Main.java
+++ SpringFrameworkDemo/src/main/java/demo/Main.java
@@ -1,11 +1,13 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         Doctor doctor = context.getBean(Doctor.class);
         Staff nurse = context.getBean(Nurse.class);
         doctor.assist();
