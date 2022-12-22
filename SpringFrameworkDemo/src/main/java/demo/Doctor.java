a1af38bee964ddd5440a01fb50fbd86d4ec15f84
mbusila-unity3d <marian.busila@unity3d.com>
2022-12-22 13:20:55 -0500

    use beans defined in application context xml

---
+++ SpringFrameworkDemo/src/main/java/demo/Doctor.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class Doctor {
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    }
+}
---
+++ SpringFrameworkDemo/src/main/java/demo/Main.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Doctor doctor = context.getBean(Doctor.class);
+        Nurse nurse = context.getBean(Nurse.class);
+        doctor.assist();
+        nurse.assist();
+    }
+}
