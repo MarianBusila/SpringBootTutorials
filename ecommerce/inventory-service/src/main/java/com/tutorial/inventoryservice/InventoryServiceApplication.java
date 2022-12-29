5dcd11aeea814f86ebb43844029912da0913615c
mbusila-unity3d <marian.busila@unity3d.com>
2022-12-29 18:11:34 -0500

    add inventory data and find by sku code

--- ecommerce/inventory-service/src/main/java/com/tutorial/inventoryservice/InventoryServiceApplication.java
+++ ecommerce/inventory-service/src/main/java/com/tutorial/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package com.tutorial.inventoryservice;
 
+import com.tutorial.inventoryservice.model.Inventory;
+import com.tutorial.inventoryservice.repository.InventoryRepository;
+import com.tutorial.inventoryservice.service.InventoryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +15,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
--- ecommerce/order-service/src/main/java/com/tutorial/orderservice/service/OrderService.java
+++ ecommerce/order-service/src/main/java/com/tutorial/orderservice/service/OrderService.java
@@ -7,6 +7,7 @@ import com.tutorial.orderservice.model.OrderLineItems;
 import com.tutorial.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest) {
