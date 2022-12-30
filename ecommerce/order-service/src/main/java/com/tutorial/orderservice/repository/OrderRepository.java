dff709a64cd1f76ce32ac1b91eb12950ca75bbb7
mbusila-unity3d <marian.busila@unity3d.com>
2022-12-29 19:15:14 -0500

    clean old projects

--- ecommerce/order-service/src/main/java/com/tutorial/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tutorial.orderservice.repository;
-
-import com.tutorial.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- ecommerce/product-service/src/main/java/com/tutorial/productservice/repository/ProductRespository.java
+++
@@ -1,7 +0,0 @@
-package com.tutorial.productservice.repository;
-
-import com.tutorial.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRespository extends MongoRepository<Product, String> {
-}
